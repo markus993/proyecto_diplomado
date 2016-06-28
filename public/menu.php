067f47805f212e35add8e861adc0482dd2eb1c6c
Marco Mayor <Marco Mayor>
2016-06-28 00:15:02 -0500

    Primer commit

---
+++ public/menu.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<!-- saved from url=(0044)http://181.48.204.173/homologaciones/public/ -->
+<html style="min-height: 591px;"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <title>Datalpha - Homologaciones</title>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <link rel="icon" type="image/png" href="http://181.48.204.173/homologaciones/public/img/favicon.ico">
+        <!-- jQuery UI 1.10.3 -->        
+		<script src="./Datalpha - Homologaciones_files/jquery-1.11.3.min.js"></script>
+        <!-- bootstrap 3.0.2 -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/bootstrap.min.css">
+        <!-- font Awesome -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/font-awesome.min.css">
+        <!-- Ionicons -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/ionicons.min.css">
+        <!-- Morris chart -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/morris.css">
+        <!-- jvectormap -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/jquery-jvectormap-1.2.2.css">
+        <!-- Daterange picker -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/daterangepicker-bs3.css">
+        <!-- bootstrap wysihtml5 - text editor -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/bootstrap3-wysihtml5.min.css">
+        <!-- Theme style -->
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/AdminLTE.css">
+
+        <link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/select2.min.css">
+		<link media="all" type="text/css" rel="stylesheet" href="./Datalpha - Homologaciones_files/style.css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+        <script type="text/javascript">
+			$( document ).ready(function() {
+                $("input[type*=text]").addClass("form-control");
+				$("input[type*=number]").addClass("form-control");
+				$("input[type*=date]").addClass("form-control");
+				$("select").addClass("form-control");
+				//$("select").select2();
+			});
+		</script>
+
+        
+    <style type="text/css">.jqstooltip { position: absolute;left: 0px;top: 0px;visibility: hidden;background: rgb(0, 0, 0) transparent;background-color: rgba(0,0,0,0.6);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)";color: white;font: 10px arial, san serif;text-align: left;white-space: nowrap;padding: 5px;border: 1px solid white;z-index: 10000;}.jqsfield { color: white;font: 10px arial, san serif;text-align: left;}</style></head>
+    <body class="skin-blue  pace-done" style="min-height: 591px;"><div class="pace  pace-inactive"><div class="pace-progress" data-progress-text="100%" data-progress="99" style="width: 100%;">
+  <div class="pace-progress-inner"></div>
+</div>
+<div class="pace-activity"></div></div>
+        <!-- header logo: style can be found in header.less -->
+        <header class="header">
+            <a href="http://181.48.204.173/homologaciones/public" class="logo">
+                <!-- Add the class icon to your logo image or logo icon to add the margining -->
+                <img alt="UNIAJC" src="./Datalpha - Homologaciones_files/logo_uniajc_azul.png" style="width:100px">
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <a href="http://181.48.204.173/homologaciones/public/#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <div class="navbar-right">
+                    <ul class="nav navbar-nav">
+                        <!-- User Account: style can be found in dropdown.less -->
+                        <li class="dropdown user user-menu">
+                            <a href="http://181.48.204.173/homologaciones/public/#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="glyphicon glyphicon-user"></i>
+                                <span>Admin <i class="caret"></i></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- User image -->
+                                <li class="user-header bg-light-blue">
+                                    <img src="./Datalpha - Homologaciones_files/avatar3.png" class="img-circle" alt="User Image">
+                                    <p>
+                                        Admin
+                                        <small>Member since Nov. 2012</small>
+                                    </p>
+                                </li>
+                                <!-- Menu Body -->
+                                <li class="user-body">
+                                    <div class="col-xs-4 text-center">
+                                        <a href="http://181.48.204.173/homologaciones/public/#">Followers</a>
+                                    </div>
+                                    <div class="col-xs-4 text-center">
+                                        <a href="http://181.48.204.173/homologaciones/public/#">Sales</a>
+                                    </div>
+                                    <div class="col-xs-4 text-center">
+                                        <a href="http://181.48.204.173/homologaciones/public/#">Friends</a>
+                                    </div>
+                                </li>
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <div class="pull-left">
+                                        <a href="http://181.48.204.173/homologaciones/public/#" class="btn btn-default btn-flat">Profile</a>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a href="http://181.48.204.173/homologaciones/public/#" class="btn btn-default btn-flat">Sign out</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div class="wrapper row-offcanvas row-offcanvas-left" style="min-height: 591px;">
+            <!-- Left side column. contains the logo and sidebar -->
+            <aside class="left-side sidebar-offcanvas" style="min-height: 591px;">
+                <!-- sidebar: style can be found in sidebar.less -->
+                <section class="sidebar">
+                    
+                    <!-- sidebar menu: : style can be found in sidebar.less -->
+                    <ul class="sidebar-menu">
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/paises">
+                                <i class="fa fa-dashboard"></i> <span>Paises</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/departamentos">
+                                <i class="fa fa-dashboard"></i> <span>Departamentos</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/ciudades">
+                                <i class="fa fa-dashboard"></i> <span>Ciudades</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/instituciones">
+                                <i class="fa fa-dashboard"></i> <span>Instituciones</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/facultades">
+                                <i class="fa fa-dashboard"></i> <span>Facultades</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/programas">
+                                <i class="fa fa-dashboard"></i> <span>Programas</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/asignaturas">
+                                <i class="fa fa-dashboard"></i> <span>Asignaturas</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/pensums">
+                                <i class="fa fa-dashboard"></i> <span>Pensums</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/documentos">
+                                <i class="fa fa-dashboard"></i> <span>Documentos</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/personas">
+                                <i class="fa fa-dashboard"></i> <span>Personas</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://181.48.204.173/homologaciones/public/homologaciones">
+                                <i class="fa fa-dashboard"></i> <span>Homologaciones</span>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+                <!-- /.sidebar -->
+            </aside>
+
+            <!-- Right side column. Contains the navbar and content of the page -->
+            <aside class="right-side">            	
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Dashboard
+                        <small>Control panel</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="http://181.48.204.173/homologaciones/public/#"><i class="fa fa-dashboard"></i> Home</a></li>
+                        <li class="active">Dashboard</li>
+                    </ol>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+					<div class="box box-info">						
+                        <div class="box-body">
+                                                    </div><!-- /.box-body -->
+                    </div>
+                	
+
+                </section><!-- /.content -->
+            </aside><!-- /.right-side -->
+        </div><!-- ./wrapper -->
+
+        
+        <!-- Bootstrap -->
+        <script src="./Datalpha - Homologaciones_files/bootstrap.min.js"></script>
+        <!-- Morris.js charts -->
+        <script src="./Datalpha - Homologaciones_files/raphael-min.js"></script>
+        <script src="./Datalpha - Homologaciones_files/morris.min.js"></script>
+        <!-- Sparkline -->
+        <script src="./Datalpha - Homologaciones_files/jquery.sparkline.min.js"></script>
+        <!-- jvectormap -->
+        <script src="./Datalpha - Homologaciones_files/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="./Datalpha - Homologaciones_files/jquery-jvectormap-world-mill-en.js"></script>
+        <!-- daterangepicker -->
+        <script src="./Datalpha - Homologaciones_files/daterangepicker.js"></script>
+        <!-- Bootstrap WYSIHTML5 -->
+        <script src="./Datalpha - Homologaciones_files/bootstrap3-wysihtml5.all.min.js"></script>
+        <!-- iCheck -->
+        <script src="./Datalpha - Homologaciones_files/icheck.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="./Datalpha - Homologaciones_files/app.js"></script>
+    
+        
+
+    
+</body></html>
